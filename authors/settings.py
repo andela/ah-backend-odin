25d2c30af2b288b67fc6badffeb2d61711c2a72a
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-10-29 09:20:01 +0300

    Remove extra bracket from  database settings

--- authors/settings.py
+++ authors/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'), 'authors'),
+        'NAME': os.environ.get('DB_NAME', 'authors'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
