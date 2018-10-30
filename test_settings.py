fe0b88b0f3e88785bd3ac0bc72f41d6c9833b6e7
wycliffkas <42246312+wycliffkas@users.noreply.github.com>
2018-10-30 15:24:22 +0300

    Ch unit tests 161340161 (#12)
    * [Chore: finishes #161340163] Upgrade app to django 2.1
    
    * [Chore:#161340163] Update requirements.txs
    
    * [Chore:#161340161] Setup unit tests
    
    * add test for login feature
    
    * change status code to 200
    
    * fix(tests): Use the right status code

--- test_settings.py
+++ test_settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'authors'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
