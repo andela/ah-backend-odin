18cc4446aa032bcd940a95f9c2a664c064a6d2ef
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-10-31 12:49:01 +0300

    [Chore:#161340161] Fix failing tests

--- authors/settings.py
+++ authors/settings.py
@@ -136,6 +136,10 @@ AUTH_USER_MODEL = 'authentication.User'
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'TEST_REQUEST_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'authors.apps.authentication.backends.JWTAuthentication',
