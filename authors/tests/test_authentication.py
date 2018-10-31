01ab6c49f7ceb71da5fcde0bb65d284770321082
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-10-31 20:44:04 +0300

    [Feature #161340173] Remove the unicode declaration

--- authors/tests/test_authentication.py
+++ authors/tests/test_authentication.py
@@ -30,50 +30,50 @@ class TestAuthentication(TestCase):
         self.create_user()
         response=self.client.post("/api/users/",{"user":{"email":"johndoe@example.com"}} )
         self.assertEqual(response.json()['errors']['email'], [
-                         u'Email already in use'])
+                        'Email already in use'])
     
     def test_existing_user_name_validation_error_message(self):
         self.create_user()
         response=self.client.post("/api/users/",{"user":{"username":"johndoe"}} )
         self.assertEqual(response.json()['errors']['username'], [
-                         u'Username already in use'])
+                        'Username already in use'])
     
     def test_password_without_an_integer_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"password":"password"}} )
         self.assertEqual(response.json()['errors']['password'], [
-                         u'Weak password. Include atleast one integer'])
+                        'Weak password. Include atleast one integer'])
     
     def test_invalid_email_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"email":"johndoeexamplecom"}} )
         self.assertEqual(response.json()['errors']['email'], [
-                         u'Enter a valid email address.'])
+                        'Enter a valid email address.'])
     
     def test_all_lower_cased_password_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"password":"password123"}} )
         self.assertEqual(response.json()['errors']['password'], [
-                         u'Password should contain both upper and lower cases'])
+                        'Password should contain both upper and lower cases'])
     
     def test_all_integer_password_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"password":"12345678"}} )
         self.assertEqual(response.json()['errors']['password'], [
-                         u'Password can not contain only integers'])
+                        'Password can not contain only integers'])
 
     def test_short_password_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"password":"pass12"}} )
         self.assertEqual(response.json()['errors']['password'], [
-                         u'Weak password. Password should be atleast 8 characters long'])
+                        'Weak password. Password should be atleast 8 characters long'])
 
     def test_password_more_than_25_characters_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"password":"PasswordOdin12345567890qtvscvgsffsgfsf"}} )
         self.assertEqual(response.json()['errors']['password'], [
-                         u'Password can not be more than 25 caharacters'])
+                        'Password can not be more than 25 caharacters'])
     
     def test_invalid_username_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"username":"@!#$%&"}} )
         self.assertEqual(response.json()['errors']['username'], [
-                         u'Invalid characters not allowed'])
+                        'Invalid characters not allowed'])
 
     def test_too_short_username_validation_error_message(self):
         response=self.client.post("/api/users/",{"user":{"username":"su"}} )
         self.assertEqual(response.json()['errors']['username'], [
-                         u'Username should be between 3 to 25 characters long'])
+                        'Username should be between 3 to 25 characters long'])
