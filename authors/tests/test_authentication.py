947388c187e8f3e2add47f00301b4d49e69cc886
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-11-01 13:46:14 +0300

    #161340161  Fix failing tests (#13)
    * [Chore: finishes #161340163] Upgrade app to django 2.1
    
    * [Chore:#161340163] Update requirements.txs
    
    * [Chore:#161340161] Setup unit tests
    
    * add test for login feature
    
    * change status code to 200
    
    * fix(tests): Use the right status code
    
    * [Chore:#161340161] Fix failing tests

--- authors/tests/test_authentication.py
+++ authors/tests/test_authentication.py
@@ -1,15 +1,33 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+from rest_framework.test import APIClient
 
 
 class TestAuthentication(TestCase):
     def setUp(self):
-        self.client = Client(content_type="application/json")
-        self.user = {
-            "username": "johndoe",
-            "password": "password",
-            "email": "johndoe@example.com"
+        self.client = APIClient(content_type="application/json")
+        self.user_data = {
+            "user": {
+                "email": "johndoe@example.com",
+                "username": "johndoe",
+                "password": "password"
+            }
         }
 
+    def create_user(self):
+        return self.client.post("/api/users/", self.user_data)
+
     def test_create_users_returns_201_status_code(self):
-        response = self.client.post("/api/users/", self.user)
+        response = self.create_user()
         self.assertEqual(response.status_code, 201)
+
+    def test_create_user_response_has_username(self):
+        response = self.create_user()
+        self.assertEqual(
+            response.data['username'],
+            self.user_data["user"]["username"]
+        )
+
+    def test_login_user_returns_200_status_code(self):
+        self.create_user()
+        response = self.client.post("/api/users/login/", self.user_data)
+        self.assertEqual(response.status_code, 200)
