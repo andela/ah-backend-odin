13c52dc88573459614ee4044657a0d717c5f9d8e
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-11-02 13:28:58 +0300

    #161340172  Include A JWT token on successful authentication (#16)
    * [Chore: finishes #161340163] Upgrade app to django 2.1
    
    * [Chore:#161340163] Update requirements.txs
    
    * [Chore:#161340161] Setup unit tests
    
    * add test for login feature
    
    * change status code to 200
    
    * fix(tests): Use the right status code
    
    * [Chore:#161340161] Fix failing tests
    
    * [Feature: #161340172] Include a JWT token on sucessfull authentication
    
    * [Feature: #161340172] Authenticate using JWT tokens instead of sessions

--- authors/tests/test_authentication.py
+++ authors/tests/test_authentication.py
@@ -16,6 +16,11 @@ class TestAuthentication(TestCase):
     def create_user(self):
         return self.client.post("/api/users/", self.user_data)
 
+      
+    def login_user(self):
+        self.create_user()
+        return self.client.post("/api/users/login/", self.user_data)
+
     def test_create_users_returns_201_status_code(self):
         response = self.create_user()
         self.assertEqual(response.status_code, 201)
@@ -26,8 +31,15 @@ class TestAuthentication(TestCase):
             response.data['username'],
             self.user_data["user"]["username"]
         )
+    def test_successfull_user_registeration_response_has_token(self):
+        response = self.create_user()
+        self.assertIn('token', response.data)
 
     def test_login_user_returns_200_status_code(self):
-        self.create_user()
-        response = self.client.post("/api/users/login/", self.user_data)
+        response = self.login_user()
         self.assertEqual(response.status_code, 200)
+
+    def test_successfull_login_response_has_token(self):
+        response = self.login_user()
+        self.assertIn('token', response.data)
+
