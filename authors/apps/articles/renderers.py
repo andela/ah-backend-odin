18994e44d41c7303cf5f55878946719db6552dd9
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-11-06 11:05:08 +0300

    [Feature #161340178] Declare the urls for the articles app

---
+++ authors/apps/articles/renderers.py
@@ -0,0 +1,19 @@
+import json
+
+from rest_framework.renderers import JSONRenderer
+
+
+
+class ArticleJSONRenderer(JSONRenderer):
+    
+    charset = 'utf-8'
+    
+    def render (self, data, media_type=None, renderer_context=None):
+        errors = data.get('errors', None)
+
+        if errors is not None:
+            return super(ArticleJSONRenderer, self).render(data)
+        
+        return json.dumps({
+            'article' : data
+        })
