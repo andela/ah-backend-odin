fa7bd6ae540c87a43a349ee7ee1092323cca7d1a
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-11-06 11:00:55 +0300

    [Feature #161340178] Add create article view

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import uuid
+from rest_framework import generics
+from .models import Article
+from .serializers import CreateArticleAPIViewSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .renderers import ArticleJSONRenderer
+from django.template.defaultfilters import slugify
+from ..authentication.backends import JWTAuthentication
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ListCreateArticleAPIView(generics.ListCreateAPIView):
+    renderer_classes = (ArticleJSONRenderer, )
+    queryset = Article.objects.all()
+    serializer_class = CreateArticleAPIViewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def create(self, request):
+        """
+            This method creates user articles
+        """
+        article = request.data.get('article', {})
+        user_info = JWTAuthentication().authenticate(request)
+        article['author'] = user_info[0]
+
+        slug = slugify(article["title"]).replace("_", "-")
+        slug = slug + "-" + str(uuid.uuid4()).split("-")[-1]
+        article["slug"] = slug
+
+        serializer = self.serializer_class(data=article)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=user_info[0])
+        data = serializer.data
+        data["message"] = "Your Article has been created successfully"
+
+        return Response(data, status=status.HTTP_201_CREATED)
