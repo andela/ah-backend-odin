cdae4ef8abf4a8340940b7a34782092aeafca0fb
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-11-06 10:59:15 +0300

    [Feature #161340178] Create articles model

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from ..authentication.models import User
+from datetime import datetime, timedelta
+
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
+
+class Article(models.Model):
+
+
+    title = models.CharField(db_index= True, max_length =255)
+    description = models.CharField(max_length =255, primary_key = True)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    published = models.BooleanField()
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager(blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.TextField(null=True, blank=True)
+
