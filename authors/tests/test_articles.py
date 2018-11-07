51ea558ff372678af4c7a7cf39f3368b8137deb8
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-11-08 01:06:38 +0300

    feat(api) create user profiles
    - create a profile every time a user is created
    - user can upload an avatar to profile
    - user can edit profile
    - user can see a given profile
    
    [Finishes: #161340177]

--- authors/tests/test_articles.py
+++ authors/tests/test_articles.py
@@ -1,6 +1,5 @@
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
-from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
+from rest_framework.utils.serializer_helpers import ReturnList
 from .base_test import BaseTest
 
 
@@ -10,26 +9,25 @@ class ArticlesTest(BaseTest):
         response = self.create_article()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIn('title', response.data)
-    
+
     def test_get_articles(self):
         response = self.get_article()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue( isinstance(response.data, ReturnList))
-    
+        self.assertTrue(isinstance(response.data, ReturnList))
+
     def test_get_one_article(self):
         response = self.get_one_article()
         self.assertEqual(response.status_code, 200)
-    
+
     def test_delete_article(self):
         self.create_article()
         response = self.delete_article()
         self.assertEqual(response.status_code, 200)
         self.assertIn("message", response.data)
-        self.assertEqual(response.json()['message'], 
-                        'Article is deleted')
-    
+        self.assertEqual(response.json()['message'],
+                         'Article is deleted')
+
     def test_update_article(self):
         self.create_article()
         response = self.update_article()
         self.assertEqual(response.status_code, 201)
-
