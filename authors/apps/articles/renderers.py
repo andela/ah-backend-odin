8d047ade47b8018cd62b936f54be8199b54874e5
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-11-07 11:10:43 +0300

    [Feature #161340178] Edit the rendererer to return a list of articles

--- authors/apps/articles/renderers.py
+++ authors/apps/articles/renderers.py
@@ -1,18 +1,27 @@
 import json
 
 from rest_framework.renderers import JSONRenderer
+from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 
 
 
 class ArticleJSONRenderer(JSONRenderer):
     
     charset = 'utf-8'
+    db_object_label = 'object'
     
     def render (self, data, media_type=None, renderer_context=None):
-        errors = data.get('errors', None)
 
-        if errors is not None:
-            return super(ArticleJSONRenderer, self).render(data)
+        if type(data) != ReturnList:
+            errors = data.get('errors', None)
+
+            if errors is not None:
+                return super(ArticleJSONRenderer, self).render(data)
+
+        if type(data) == ReturnDict:
+            return json.dumps({
+                self.db_object_label: data
+            })
         
         return json.dumps({
             'article' : data
