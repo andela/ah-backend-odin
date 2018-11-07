636f60ba06707f76e0a59d12e96935f92f6aefb1
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-11-07 11:16:25 +0300

    [Feature #161340178] Add path to update article view

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 
 from .views import (
     ListCreateArticleAPIView,
+    UpdateDestroyArticleAPIView,
 )
 
 urlpatterns = [
     path('', ListCreateArticleAPIView.as_view()),
-
+    path('<slug>', UpdateDestroyArticleAPIView.as_view(), )
 ]
