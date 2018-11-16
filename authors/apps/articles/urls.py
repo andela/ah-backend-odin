2b7572dff4da51b2db6ae3eed28d443a4817a426
JSnakegitHub <nyekowalter69@gmail.com>
2018-11-16 10:14:22 +0300

    feat (api): allow users to rate articles
    - Users can rate others' articles
    - Users cannot rate their own articles
    - Users cannot rate an article twice or more than once
    
    [Finishes #161340179]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', ListCreateArticleAPIView.as_view()),
     path('<slug>', UpdateDestroyArticleAPIView.as_view(), ),
     path('<slug>/bookmark', BookMarkArticleAPIView.as_view(), ),
-    path('<slug>/likes', LikeArticleAPIView.as_view(), )
+    path('<slug>/likes', LikeArticleAPIView.as_view(), ),
     path('<slug>/ratings/', CreateRatings.as_view()),
     path('<slug>/viewratings/', RetrieveRatings.as_view() ),
     path('ratings/', RetrieveAllArticlesWithRatings.as_view() ),
